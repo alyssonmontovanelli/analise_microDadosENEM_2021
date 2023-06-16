30eb73eef3439e3cef8583252935d11f9ac00e71
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2023-06-15 21:07:43 -0300

    inicio projeto

---
+++ filtroDados/utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+#panorama presença/faltas por região/sexo/raça
+colPresenca = ['NU_INSCRICAO', 'TP_SEXO', 'TP_COR_RACA',
+                   'SG_UF_PROVA', 'TP_PRESENCA_MT', 'TP_PRESENCA_LC']
+
+#panorama tipo de escola / nota final / redação
+colEscola_nota = ['NU_INSCRICAO', 'TP_ESCOLA', 'NU_NOTA_CN',
+                  'NU_NOTA_CH', 'NU_NOTA_CH', 'NU_NOTA_MT'
+                  'NU_NOTA_REDACAO']
+
+#panorama das principais dificuldades na prova de redação para alunos\
+#de rede publica e privada
+col_redacao = ['TP_ESCOLA', 'TP_STATUS_REDACAO', 'NU_NOTA_COMP1',
+               'NU_NOTA_COMP2', 'NU_NOTA_COMP3', 'NU_NOTA_COMP4',
+               'NU_NOTA_COMP5']
